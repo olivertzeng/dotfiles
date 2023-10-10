7c89308671481f1f5ed27f1937d66d06cf55ea25
Oliver Tzeng <olivertzeng@proton.me>
2023-10-10 20:48:22 +0800

    hi

--- backup.sh
+++ backup.sh
@@ -1,17 +1,24 @@
-sudo pacman -Syu
-sudo pacman -Sy - < packages/pkglist.txt
-git clone https://aur.archlinux.org/yay
-cd yay-git
+git clone https://aur.archlinux.org/yay.git
+cd yay
 makepkg -si      
 cd
+rm -rf yay
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-yay -Sy - < packages/aurlist.txt
-yay -Syu
-git clone git@github.com:olivertzeng/dotfiles.git
+git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
+yay -Syyu - < ~/dotfiles/packages/aurlist.txt
+git clone https://github.com/olivertzeng/dotfiles.git
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp dotfiles/vimrc ~/.vimrc
-cp dotfiles/zshrc ~/.zshrc
-sudo cp dotfiles/paccache.timer /dotfiles /etc/systemd/system/paccache.timer
-sudo cp dotfiles/paccache.hook /usr/share/libalpm/hooks/paccache.hook
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+bash -c  "$(wget -qO- https://git.io/vQgMr)"
+cp ~/dotfiles/vimrc ~/.vimrc
+cp ~/dotfiles/zshrc ~/.zshrc
+cp ~/dotfiles/zsh_plugins.txt ~/.zsh_plugins.txt
+sudo cp ~/dotfiles/pacman.conf /etc/
+sudo cp ~/dotfiles/paccache.timer /etc/systemd/system/paccache.timer
+sudo cp ~/dotfiles/paccache.hook /usr/share/libalpm/hooks/paccache.hook
+sudo pacman -Syyu - < ~/dotfiles/packages/pkglist.txt
+p10k configure
+sudo ufw enable
+sudo ufw allow 1714:1764/udp
+sudo ufw allow 1714:1764/tcp
+sudo ufw reload
