7c9e00b06eb2b4721fa440118686551d539fb59a
Oliver Tzeng <olivertzeng@proton.me>
2023-11-06 13:04:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@ pacman -S gum
 cd
 timedatectl set-ntp true
 cfdisk /dev/nvme0n1 || exit 1
+umount -a
 gum confirm "Do you want to continue installing Arch?" || exit 0
 mkfs.fat -F32 /dev/nvme0n1p1
-mkfs.btrfs /dev/nvme0n1p3
-mkswap /dev/nvme0n1p2
-swapon /dev/nvme0n1p2
-mount /dev/nvme0n1p3 /mnt
+mkfs.btrfs -f /dev/nvme0n1p3 || exit 1
+mkswap /dev/nvme0n1p2 || exit 1
+swapon /dev/nvme0n1p2 || exit 1
+mount /dev/nvme0n1p3 /mnt || exit 1
 mkdir -p /mnt/boot/efi
 mount /dev/nvme0n1p1 /mnt/boot/efi
 pacman -Syy --noconfirm reflector
@@ -18,16 +19,18 @@ pacstrap -K /mnt base linux linux-firmware linux-headers amd-ucode neovim refind
 genfstab -U /mnt >> /mnt/etc/fstab
 gum confirm "Do Arch chroot?" || exit 0
 arch-chroot /mnt
-pacman --noconfirm - < ~/dotfiles/packages/pkglist.txt
-pacman -Sc
-pacman -Scc
 git clone https://github.com/olivertzeng/dotfiles
+pacman --noconfirm - < dotfiles/packages/pkglist.txt || exit 1
+gum confirm "Are packages fine?" || exit 1
+yes y | pacman -Sc
+yes y | pacman -Scc
+curl -s 'https://liquorix.net/install-liquorix.sh' | sh
 ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 hwclock --systohc
 passwd
 useradd -m -G wheel,audio,video,storage -s /usr/bin/zsh olivertzeng
 passwd olivertzeng
-echo "Please uncomment the %wheel ALL=(ALL) ALL"
+echo "Please uncomment %wheel ALL=(ALL) ALL"
 sleep 5
 EDITOR=nvim visudo
 su olivertzeng
@@ -40,14 +43,13 @@ rm -rf yay
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 yay -Syyu --noconfirm - < ~/dotfiles/packages/aurlist.txt
+gum confirm "Are AUR okay?" || exit 1
 yes y | yay -Sc
 yes y | yay -Scc
-git clone https://github.com/olivertzeng/dotfiles.git
 cp ~/dotfiles/vimrc ~/.vimrc
 cp ~/dotfiles/zshrc ~/.zshrc
 cp ~/dotfiles/zsh_plugins.txt ~/.zsh_plugins.txt
 cp ~/dotfiles/init.lua ~/.config/nvim/init.lua
-curl -s 'https://liquorix.net/install-liquorix.sh' | sh
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.zprofile
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 zsh
@@ -56,7 +58,7 @@ cargo install cargo-cache
 p10k configure
 exit
 exit
-cp ~/dotfiles/templates ~/.config/nvim/templates
+cp -r ~/dotfiles/templates ~/.config/nvim/templates
 cp ~/dotfiles/pacman.conf /etc/
 cp ~/dotfiles/paccache.timer /etc/systemd/system/paccache.timer
 cp ~/dotfiles/paccache.hook /usr/share/libalpm/hooks/paccache.hook
