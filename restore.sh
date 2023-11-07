d5c6108ce3860725945775c624b5bc729deee584
Oliver Tzeng <olivertzeng@proton.me>
2023-11-07 11:12:35 +0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,22 +1,3 @@
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si      
-cd
-rm -rf yay
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
-yay -Syyu - < ~/dotfiles/packages/aurlist.txt
-git clone https://github.com/olivertzeng/dotfiles.git
-bash -c  "$(wget -qO- https://git.io/vQgMr)"
-cp ~/dotfiles/vimrc ~/.vimrc
-cp ~/dotfiles/zshrc ~/.zshrc
-cp ~/dotfiles/zsh_plugins.txt ~/.zsh_plugins.txt
-cp ~/dotfiles/init.lua ~/.config/nvim/init.lua 
-sudo cp ~/dotfiles/pacman.conf /etc/
-sudo cp ~/dotfiles/paccache.timer /etc/systemd/system/paccache.timer
-sudo cp ~/dotfiles/paccache.hook /usr/share/libalpm/hooks/paccache.hook
-sudo pacman -Syyu - < ~/dotfiles/packages/pkglist.txt
-p10k configure
 sudo ufw enable
 sudo ufw allow 1714:1764/udp
 sudo ufw allow 1714:1764/tcp
