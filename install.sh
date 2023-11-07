809efa6810569c8981f34141670ae0eabe24f82e
Oliver Tzeng <olivertzeng@proton.me>
2023-11-07 11:33:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,34 +37,6 @@ passwd olivertzeng
 echo "Please uncomment %wheel ALL=(ALL) ALL"
 sleep 5
 EDITOR=nvim visudo
-
-# login as user olivertzeng
-su olivertzeng
-cd
-git clone https://github.com/olivertzeng/dotfiles.git &
-git clone https://aur.archlinux.org/yay.git &
-git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote &
-wait
-cd yay
-makepkg -si      
-cd ..
-rm -rf yay
-yes "" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-yay -Syyu --noconfirm - < ~/dotfiles/packages/aurlist.txt
-gum confirm "Are AUR okay?" || exit 1
-yes | yay -Sc
-yes | yay -Scc
-cp ~/dotfiles/zshrc ~/.zshrc
-cp ~/dotfiles/zsh_plugins.txt ~/.zsh_plugins.txt
-cp ~/dotfiles/init.lua ~/.config/nvim/init.lua
-cp -r ~/dotfiles/templates ~/.config/nvim/templates
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.zprofile
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-zsh
-brew install powerlevel10k gcc
-cargo install cargo-cache
-exit
-exit
 nvim /etc/locale.gen
 echo "LANG=zh_TW.UTF-8" > /etc/locale.conf
 echo "ArchBTW" > /etc/hostname
