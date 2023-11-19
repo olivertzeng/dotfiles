bdc1039e8d4ec096373a463a2096df02301a1f4d
Oliver <olivertzeng@proton.me>
2023-11-19 10:02:56 +0800

    bruh

--- restore.sh
+++ restore.sh
@@ -1,23 +1,11 @@
-cd
-git clone https://aur.archlinux.org/yay.git &
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote &
-wait
-cd yay
-makepkg -si      
-cd ..
-rm -rf yay
-yes "" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-yay -Syyu --noconfirm $(cat -/dotfiles/packages/aurlist.txt | xargs)
 yay -S --rebuild --answerclean A --answerdiff N $(checkrebuild | cut -d$'\t' -f2)
-gum confirm "Are AUR okay?" || exit 1
 yes | yay -Sc
 yes | yay -Scc
 cp ~/dotfiles/zshrc ~/.zshrc
 cp ~/dotfiles/zsh_plugins.txt ~/.zsh_plugins.txt
 cp ~/dotfiles/init.lua ~/.config/nvim/
 cp -r ~/dotfiles/templates ~/.config/nvim/templates
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.zprofile
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 bash -c  "$(wget -qO- https://git.io/vQgMr)"
 zsh
 brew install powerlevel10k gcc
