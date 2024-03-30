91ff4683441f5e4cd9bbd7861a00b5b3991a5718
Oliver Tzeng <olivertzeng@proton.me>
2024-03-30 10:50:24 +0800

    removed my deprecated nvim configs

--- nvim.bak/nvim/templates/cpp.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright {{_lua:os.date("%y/%m/%d %H/%M")_}} {{_author_}}. All rights reserved.
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(int argc, char *argv[]){
-    {{_cursor_}}
-    return 0;
-}
--- nvim.bak/templates/main.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright {{_date_}} {{_author_}}. All rights reserved.
-#include <iostream>
-using namespace std;
-
-int main () {
-
-}
