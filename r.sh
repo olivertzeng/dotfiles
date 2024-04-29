67d8f893f998826968e5b7a12d462076e2d4bb4f
Oliver Tzeng <olivertzeng@proton.me>
2024-04-29 19:49:11 +0800

    bruh

--- r.sh
+++ r.sh
@@ -4,7 +4,6 @@ makepkg -si
 cd ..
 rm -rf yay-git
 yay --needed -Syu $(cat packages/aurlist.txt | xargs)
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 yes | yay -Sc
 yes | yay -Scc
@@ -12,4 +11,4 @@ cp zshrc ~/.zshrc
 cp zsh_plugins.txt ~/.zsh_plugins.txt
 cp -r nvim ~/.config
 source ~/.zshrc
-brew install powerlevel10k pipx gcc
+rm ~/.gnupg/{S.keyboxd,public-keys.d/pubring.db.lock}
