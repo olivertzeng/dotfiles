3f9c7800e9d7a777449fed1dc8a1ff83691a403b
Oliver Tzeng <olivertzeng@proton.me>
2024-05-22 15:31:58 +0800

    dots

--- r.sh
+++ r.sh
@@ -10,5 +10,13 @@ yes | yay -Scc
 cp zshrc ~/.zshrc
 cp zsh_plugins.txt ~/.zsh_plugins.txt
 cp -r nvim ~/.config
+cp clang-format ~/.clang-format
+cp activate-linux.sh ~/
+cp gitconfig ~/.gitconfig
+cp dark.sh ~/
+cp light.sh ~/
+cp zshenv ~/.zshenv
+cp topgrade.toml ~/.config
+cp pip.conf ~/.config/pip
 source ~/.zshrc
 rm ~/.gnupg/{S.keyboxd,public-keys.d/pubring.db.lock}
