18766460f171e573e2184ec07a8dbfd9ab2062b0
Oliver Tzeng <olivertzeng@proton.me>
2024-09-21 22:52:10 +0800

    bruh

--- theme.sh
+++ theme.sh
@@ -11,13 +11,15 @@ if awk "BEGIN {exit !($lon < 0)}"; then lon_dir="W"; fi
 if [ "$(sunwait poll ${lat}${lat_dir} ${lon}${lon_dir})" = "DAY" ]; then
 	plasma-apply-colorscheme 'Gruvbox Light - Red two-tone 3'
 	plasma-apply-cursortheme miku-cursor-linux >/dev/null 2>&1
-	/usr/lib/plasma-changeicons BeautySolar >/dev/null 2>&1
+	# /usr/lib/plasma-changeicons BeautySolar >/dev/null 2>&1
+	/usr/lib/plasma-changeicons besgnulinux-colors >/dev/null 2>&1
 	kvantummanager --set Gruvbox_Light_Green
 	plasma-apply-wallpaperimage ~/.local/share/wallpapers/boiling-microsoft-light.png
 else
 	plasma-apply-colorscheme GruvboxColors
 	plasma-apply-cursortheme Bibata-Rainbow-Modern >/dev/null 2>&1
-	/usr/lib/plasma-changeicons BeautyLine >/dev/null 2>&1
+	# /usr/lib/plasma-changeicons BeautyLine >/dev/null 2>&1
+	/usr/lib/plasma-changeicons Gruvbox-Plus-Dark >/dev/null 2>&1
 	kvantummanager --set Gruvbox-Dark-Brown
 	plasma-apply-wallpaperimage ~/.local/share/wallpapers/boiling-microsoft-dark.png
 fi
