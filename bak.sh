04309d01562bffff9c53774a77b934a43fee2bed
Oliver Tzeng <olivertzeng@proton.me>
2025-05-28 20:30:33 +0800

    added a lot of stuff

---
+++ bak.sh
@@ -0,0 +1,13 @@
+cp -r ~/.config/nvim ~/dotfiles
+cp /etc/makepkg.conf ~/dotfiles
+cp /etc/pacman.conf ~/dotfiles
+cp ~/.clang-format ~/dotfiles/clang-format
+cp ~/.config/pip/pip.conf ~/dotfiles
+cp ~/.config/topgrade.toml ~/dotfiles
+cp ~/.gitconfig ~/dotfiles/gitconfig
+cp ~/.zsh_plugins.txt ~/dotfiles/zsh_plugins.txt
+cp ~/.zshbookmarks ~/dotfiles/zshbookmarks
+cp ~/.zshrc ~/dotfiles/zshrc
+cp ~/bak.sh ~/dotfiles
+cp ~/dark.sh ~/dotfiles
+cp ~/light.sh ~/dotfiles
