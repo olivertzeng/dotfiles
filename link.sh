8afef8b943fd49cb0ebb4dc345925d7ccf68ee42
Oliver Tzeng <olivertzeng@proton.me>
2026-02-01 14:23:05 +0800

    new stuff

--- link.sh
+++ link.sh
@@ -1,13 +1,13 @@
-ln -sf -r ~/dotfiles ~/.config/nvim
-ln -sf ~/dotfiles ~/.config/pip/pip.conf
-ln -sf ~/dotfiles ~/.config/topgrade.toml
-ln -sf ~/dotfiles/link.sh ~/
 ln -sf ~/dotfiles/clang-format ~/.clang-format
 ln -sf ~/dotfiles/dark.sh ~/
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/light.sh ~/
+ln -sf ~/dotfiles/link.sh ~/
+ln -sf ~/dotfiles/nvim ~/.config
+ln -sf ~/dotfiles/pip.conf ~/.config/pip
 ln -sf ~/dotfiles/replace.sh ~/
 ln -sf ~/dotfiles/shift.sh ~/
+ln -sf ~/dotfiles/topgrade.toml ~/.config
 ln -sf ~/dotfiles/yay.json ~/.config/yay/config.json
 ln -sf ~/dotfiles/yt-dlp ~/.config/yt-dlp/config
 ln -sf ~/dotfiles/yt.sh ~/
